c630fe9d3000a2daa74be0bdee937462668e6d57
DeFiDude <59237470+DeFiDude@users.noreply.github.com>
2026-03-10 00:53:48 -0600

    Bump version to 1.5.8

--- src/config/Config.h
+++ src/config/Config.h
@@ -6,8 +6,8 @@
 
 #define RATDECK_VERSION_MAJOR  1
 #define RATDECK_VERSION_MINOR  5
-#define RATDECK_VERSION_PATCH  7
-#define RATDECK_VERSION_STRING "1.5.7"
+#define RATDECK_VERSION_PATCH  8
+#define RATDECK_VERSION_STRING "1.5.8"
 
 // --- Feature Flags ---
 #define HAS_DISPLAY     true
